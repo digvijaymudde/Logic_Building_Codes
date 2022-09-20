1a0685d23bebed8f8161be5c85fde3c7fcc4c513
digvijay mudde <90601525+digvijaymudde@users.noreply.github.com>
2022-09-20 19:03:30 +0530

    Create progaram3.c

---
+++ progaram3.c
@@ -0,0 +1,20 @@
+# Logic_Building_Codes
+Simple level to difficult problem stetment codes
+//program return 5 to 1 number 
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt=0;
+    for(iCnt=5;iCnt>0;iCnt--)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
